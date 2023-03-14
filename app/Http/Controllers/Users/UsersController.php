bda61d0623e7369570f6ef92f46636d326a119f7
obeyda <aldersonzero@gmail.com>
2023-03-14 23:43:10 +0300

    upload project

--- app/Http/Controllers/Users/UsersController.php
+++ app/Http/Controllers/Users/UsersController.php
@@ -22,7 +22,7 @@ class UsersController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['']]); //
+        $this->middleware('auth:api', ['except' => ['']]);
     }
     public function OfType($type)
     {
